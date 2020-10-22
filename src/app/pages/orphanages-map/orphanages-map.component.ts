779e52e3f3d03006c0b066ed2f7549df41f793b6
GentilPinto <gentil.junior.gp@gmail.com>
2020-10-22 14:37:48 +0100

    [frontend] corrected the method to get my location :smile:

--- src/app/pages/orphanages-map/orphanages-map.component.ts
+++ src/app/pages/orphanages-map/orphanages-map.component.ts
@@ -1,37 +1,74 @@
 import { Component, OnInit } from '@angular/core';
-import { latLng, tileLayer } from 'leaflet';
+import {
+    latLng,
+    tileLayer,
+    marker,
+    icon
+} from 'leaflet';
+import * as Leaflet from 'leaflet';
 
 @Component({
-  selector: 'app-orphanages-map',
-  templateUrl: './orphanages-map.component.html',
-  styleUrls: ['./orphanages-map.component.scss']
+    selector: 'app-orphanages-map',
+    templateUrl: './orphanages-map.component.html',
+    styleUrls: ['./orphanages-map.component.scss']
 })
 export class OrphanagesMapComponent implements OnInit {
 
-  private _options = {
-    layers: [
-      tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
-    ],
-    zoom: 5,
-    center: latLng(0, 0)
-  };
+    private _myCurrentPosition = {
+        latitude: null,
+        longitude: null
+    };
 
-  constructor() { }
+    private _map: Leaflet.Map;
 
-  ngOnInit(): void {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(this.setPosition);
-    } else {
-      this._options.center = latLng(-8.9179403, 13.200951799999999);
+    private _options = {
+        layers: [
+            tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '' })
+        ],
+        zoom: 15,
+        center: latLng(0, 0)
+    };
+
+    layer = marker([-8.9179403, 13.200951799999999], {
+        icon: icon({
+            iconSize: [58, 68],
+            iconAnchor: [29, 68],
+            iconUrl: 'assets/images/map-marker.svg'
+        })
+    });
+
+    constructor() { }
+
+    ngOnInit(): void { }
+
+    public get options(): any {
+        return this._options;
     }
-  }
 
-  public setPosition(position): void {
-    this._options.center = latLng(position.coords.latitude, position.coords.longitude);
-  }
+    public async onMapReady(map: Leaflet.Map): Promise<void> {
+        await this.getMyCurrentPosition().then((result) => {
+            this._myCurrentPosition.latitude = result.coords.latitude;
+            this._myCurrentPosition.longitude = result.coords.longitude;
+        });
+        this._map = map.panTo(
+            new Leaflet.LatLng(
+                this._myCurrentPosition.latitude,
+                this._myCurrentPosition.longitude
+            )
+        );
+    }
 
-  public get options(): object {
-    return this._options;
-  }
-
+    public getMyCurrentPosition(): Promise<any> {
+        return new Promise((resolve, reject) => {
+            navigator.geolocation.getCurrentPosition(
+                (result) => {
+                    resolve(result);
+                },
+                (error) => {
+                    console.log('Error: ', error);
+                    reject(error);
+                }
+            );
+        });
+    }
 }

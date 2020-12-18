0eeccb51fe20ebeddfdcbce666f700402b413fdb
GentilPinto <gentil.junior.gp@gmail.com>
2020-12-18 18:55:21 +0100

    add file to setup map marker icon

---
+++ src/app/shared/marker-icon.ts
@@ -0,0 +1,10 @@
+import { icon } from 'leaflet';
+
+export const MarkerIcon = {
+    icon: icon({
+        iconUrl: 'assets/images/map-marker.svg',
+        iconSize: [58, 68],
+        iconAnchor: [29, 68],
+        popupAnchor: [0, -60]
+    })
+};

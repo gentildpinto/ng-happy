8b7bdc4f7143d74e7f9204bc301904c436b8894d
Gentil Pinto <gentil.junior.gp@gmail.com>
2021-12-16 11:34:27 +0100

    updated orphanage service

--- src/app/services/oprhanage.service.ts
+++ src/app/services/oprhanage.service.ts
@@ -3,48 +3,49 @@ import { Observable, throwError } from 'rxjs';
 import { map, retry, catchError } from 'rxjs/operators';
 import { Orphanage } from '../interfaces/orphanage.interface';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
-    providedIn: 'root'
+	providedIn: 'root'
 })
 export class OprhanageService {
 
-    private _backendURL = 'http://localhost:3000/api/v1/';
+	private _backendURL: string = environment.apiUrl;
 
-    private _httpOptions = {
-        headers: new HttpHeaders({
-            'Content-Type': 'application/json'
-        })
-    };
+	private _httpOptions = {
+		headers: new HttpHeaders({
+			'Content-Type': 'application/json'
+		})
+	};
 
-    constructor(
-        private _http: HttpClient
-    ) { }
+	constructor(
+		private _http: HttpClient
+	) { }
 
-    public getAllOrphanages(): Observable<Orphanage[]> {
-        return this._http.get<Orphanage[]>(this._backendURL + 'orphanages')
-            .pipe(
-                retry(3),
-                map((object: any) => object.orphanages),
-                catchError(this.handleError)
-            );
-    }
+	public getAllOrphanages(): Observable<Orphanage[]> {
+		return this._http.get<Orphanage[]>(this._backendURL + 'orphanages')
+			.pipe(
+				retry(3),
+				map((object: any) => object.orphanages),
+				catchError(this.handleError)
+			);
+	}
 
-    public storeOrphanage(orphanage: Orphanage): Observable<Orphanage> {
-        return this._http.post<Orphanage>(this._backendURL + 'orphanages', JSON.stringify(orphanage), this._httpOptions)
-            .pipe(
-                retry(3),
-                catchError(this.handleError)
-            );
-    }
+	public storeOrphanage(orphanage: Orphanage): Observable<Orphanage> {
+		return this._http.post<Orphanage>(this._backendURL + 'orphanages', JSON.stringify(orphanage), this._httpOptions)
+			.pipe(
+				retry(3),
+				catchError(this.handleError)
+			);
+	}
 
-    private handleError(error: any): Observable<never> {
-        const errorMessage = error.error instanceof ErrorEvent
-            ? error.error.message
-            : `Error Code: ${error.status}
+	private handleError(error: any): Observable<never> {
+		const errorMessage = error.error instanceof ErrorEvent
+			? error.error.message
+			: `Error Code: ${error.status}
                Message: ${error.message}`;
 
-        // window.alert(errorMessage);
-        return throwError(errorMessage);
-    }
+		// window.alert(errorMessage);
+		return throwError(errorMessage);
+	}
 }

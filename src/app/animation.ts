79e4c15be1973f0c1430f0797dae9bd6c07b441a
Gentil Pinto <gentil.junior.gp@gmail.com>
2021-12-20 11:46:05 +0100

    update animations

--- src/app/animation.ts
+++ src/app/animation.ts
@@ -1,16 +1,14 @@
 import {
 	trigger,
-	state,
 	style,
 	animate,
 	transition,
 	query,
 	group,
-	animateChild
 } from '@angular/animations';
 
 export const slideInAnimation = trigger('routeAnimations', [
-	transition('HomePage => *', [
+	transition('HomePage => OrphanagesPage', [
 		query(
 			':enter, :leave',
 			style({ position: 'fixed', width: '100%' }),
@@ -42,6 +40,67 @@ export const slideInAnimation = trigger('routeAnimations', [
 		]),
 	]),
 	transition('OrphanagesPage => HomePage', [
+		query(
+			':enter, :leave',
+			style({ position: 'fixed', width: '100%' }),
+			{ optional: true }
+		),
+		group([
+			query(
+				':enter',
+				[
+					style({ transform: 'translateX(-100%)' }),
+					animate(
+						'0.5s ease-in-out',
+						style({ transform: 'translateX(0%)' })
+					),
+				],
+				{ optional: true }
+			),
+			query(
+				':leave',
+				[
+					style({ transform: 'translateX(0%)' }),
+					animate(
+						'0.5s ease-in-out',
+						style({ transform: 'translateX(100%)' })),
+				],
+				{ optional: true }
+			),
+		]),
+	]),
+	transition('OrphanagesPage => CreateOrphanagePage', [
+		query(
+			':enter, :leave',
+			style({ position: 'fixed', width: '100%' }),
+			{ optional: true }
+		),
+		group([
+			query(
+				':enter',
+				[
+					style({ transform: 'translateX(100%)' }),
+					animate(
+						'0.5s ease-in-out',
+						style({ transform: 'translateX(0%)' })
+					),
+				],
+				{ optional: true }
+			),
+			query(
+				':leave',
+				[
+					style({ transform: 'translateX(0%)' }),
+					animate(
+						'0.5s ease-in-out',
+						style({ transform: 'translateX(-100%)' })
+					),
+				],
+				{ optional: true }
+			),
+		]),
+	]),
+	transition('CreateOrphanagePage => OrphanagesPage', [
 		query(
 			':enter, :leave',
 			style({ position: 'fixed', width: '100%' }),
